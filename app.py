35cb6ea20e29ebbd4f726bb94d93d34594f9f4da
Gabriel Yamashita <gabriel.yamashita01@gmailcom>
2021-09-28 23:49:58 -0300

    isoweekday System.Object[] working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Imports:
+## Imports:
 import json
 import time
 import os
@@ -8,65 +8,62 @@ from twilio.rest import Client
 from dotenv import load_dotenv
 
 
-# Settings:
-account_sid = os.environ.get('ACCOUNT_SID')
-auth_token = os.environ.get('AUTH_TOKEN')
+## Settings:
+account_sid = os.environ.get('ACCOUNT_SID') # account sid para o heroku
+auth_token = os.environ.get('AUTH_TOKEN') # auth token para o heroku
 
-# load_dotenv()
-# account_sid = os.getenv('ACCOUNT_SID')
-# auth_token = os.getenv('AUTH_TOKEN')
+# load_dotenv() # carrega as variáveis de ambiente
+# account_sid = os.getenv('ACCOUNT_SID') # account sid para rodar localmente
+# auth_token = os.getenv('AUTH_TOKEN') # auth token para rodar localmente
 client = Client(account_sid, auth_token) # inicializa cliente do Twilio
 
 
-# Funções:
+## Funções:
 def check(diaSemana, tempo, data): # checa os parâmetros da hora atual, e depois manda o lembrete
-    for info in data['YABOT']:
-        dia_mes = info['dia_mes']
-        date = info['dias_num']
-        schedule = info['horario']
-        message = info['message']
+    for info in data['YABOT']: # checa cada info de data
+        dia_mes = info['dia_mes'] # recebe o dia do mês
+        date = info['dias_num'] # recebe o dia da semana
+        schedule = info['horario'] # recebe o horário
+        message = info['message'] # recebe a mensagem
+        
         for i in date:
             if i == diaSemana or i == 0: # checa o dia da semana
                 if tempo == schedule: # checa o horário atual
                     manda_mensagem(message, tempo)
 
-def manda_mensagem(mensagem, tempo): # função para mandar mensagem
+def manda_mensagem(mensagem, tempo): # função para mandar a mensagem
     from_whatsapp_number = 'whatsapp:+14155238886'
     to_whatsapp_number = 'whatsapp:+5511991982436'
 
     message = client.messages.create(body=mensagem,
                                      from_=from_whatsapp_number,
                                      to=to_whatsapp_number)
-    print(f'SID: {message.sid}, \nTIME: {tempo}')
+    # print(f'SID: {message.sid}, \nTIME: {tempo}')
 
 def add_json():
-    # if msg.lower() in ["add"]:
-    #     manda_mensagem("teste add")
     pass
 
 
-# Main():
+## Main():
 def main():
-    # Tempo:
-    utc = datetime.datetime.now(datetime.timezone.utc)
-    BRSP = timezone('America/Sao_Paulo')
-    now = utc.astimezone(BRSP)
-
+    ## Tempo:
+    utc = datetime.datetime.now(datetime.timezone.utc) # setando o tempo para UTC
+    BRSP = timezone('America/Sao_Paulo') # escolhendo o fuso horário
+    now = utc.astimezone(BRSP) # adicionando o fuso horário de SP
     segundo = now.second
 
     if segundo == 0:
-        # Load Data:
+        # Carregando o JSON:
         with open('messages.json') as f:
-            data = json.load(f)
+            data = json.load(f) # armazenando na variável data
 
         # Variáveis:
-        diaSemana = now.isoweekday()
-        tempo = now.strftime('%H:%M')
+        diaSemana = now.isoweekday() # dia da semana atual
+        tempo = now.strftime('%H:%M') # horário atual
         
         # Função:
         print(f"TIME: {tempo}")
         check(diaSemana, tempo, data)
-        print("")
 
         # Espera 1 segundo:
         time.sleep(1)
